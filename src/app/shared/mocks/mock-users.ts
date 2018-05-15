b0ac5892f1ab51dd198f5797bcdcf4aa8c2366d5
oovg <office@vengist.com>
2018-05-15 16:23:44 -0700

    completes all tribes, projects, votes, tasks, and user mocks. adds curators to tribe-detail page. add rewards to task cards. add images to votes/tasks

--- src/app/shared/mocks/mock-users.ts
+++ src/app/shared/mocks/mock-users.ts
@@ -1,13 +1,13 @@
 import { User } from '../../user/user';
 
 export const USERS: User[] = [
-  { id: '0xb856e7847e5A41F6459a0687FF4FE5E93eE60F16', name: 'Mr. Nice', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x777f7b8328412f10604E45395E00027272122531', name: 'Narco', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0xc8C435CD8e48070eb21c6C3952DfFA7516400a08', name: 'Bombasto', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x14', name: 'Celeritas', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x15', name: 'Magneta', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x16', name: 'RubberMan', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x17', name: 'Dynama', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x18', name: 'Dr IQ', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x19', name: 'Magma', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
-  { id: '0x20', name: 'Tornado', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] }]
+  { id: '0xb856e7847e5A41F6459a0687FF4FE5E93eE60F16', image: 'assets/users/jaiia.jpg', name: 'Jaiia', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x777f7b8328412f10604E45395E00027272122531', image: 'assets/users/dt.jpg', name: 'DT', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0xc8C435CD8e48070eb21c6C3952DfFA7516400a08', image: 'assets/users/msc.png', name: 'MSC', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x14', image: 'assets/users/user_jaiia.png', name: 'Celeritas', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x15', image: 'assets/users/user_jaiia.png', name: 'Magneta', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x16', image: 'assets/users/user_jaiia.png', name: 'RubberMan', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x17', image: 'assets/users/user_jaiia.png', name: 'Dynama', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x18', image: 'assets/users/user_jaiia.png', name: 'Dr IQ', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x19', image: 'assets/users/user_jaiia.png', name: 'Magma', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] },
+  { id: '0x20', image: 'assets/users/user_jaiia.png', name: 'Tornado', email: '', ethBalance: 10, ntBalance: 0, ttBalance: 0, tribeTokens: [] }]
