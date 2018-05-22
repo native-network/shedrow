93a999fdb8347494ec97d6f5189fd50d617ffa68
Dekan <dekanbro@gmail.com>
2018-05-22 15:01:46 -0600

    Development (#34)
    * .
    
    * Feat joindiag (#12)
    
    * Feat joinsteps (#16)
    
    * add mat-stepper
    
    * remove mat stepper
    
    * adds confetti and some join-dialog copy (#14)
    
    * step starter and some dialog fixes (#15)
    
    * quick value check
    
    * add authguard (#17)
    
    * listen to current user (#18)
    
    * Ui converter (#19)
    
    * emitters and buttons for token-converter and other dialogs
    
    * better formatting for converter
    
    * more formatting for converter
    
    * Ui layouts (#20)
    
    * updates to tribe-card, tribe-list, tribe-detail layouts
    
    * add blocky to tribe-detail
    
    * Ui layouts (#22)
    
    * updates to tribe-card, tribe-list, tribe-detail layouts
    
    * add blocky to tribe-detail
    
    * ui_layouts
    
    * adds NT cost ratio for tribe membership on cards. fix dialog height for better overflow:auto
    
    * adds confetti to project detail (#23)
    
    * ratio conversions (#25)
    
    * cost to join the normalized across tribe-card states/sides (#27)
    
    * Feat joinwizard (#28)
    
    * refactor join-wizard
    
    * starter polish on new join wizard
    
    * polish join-wizard
    
    * add bank (#32)
    
    * Feat votestuff (#31)
    
    * start vote actions
    
    * add results
    
    * add results
    
    * add related models
    
    * has voted on function
    
    * Ui votestuff (#29)
    
    * Development (#21)
    
    * .
    
    * Feat joindiag (#12)
    
    * Feat joinsteps (#16)
    
    * add mat-stepper
    
    * remove mat stepper
    
    * adds confetti and some join-dialog copy (#14)
    
    * step starter and some dialog fixes (#15)
    
    * quick value check
    
    * add authguard (#17)
    
    * listen to current user (#18)
    
    * Ui converter (#19)
    
    * emitters and buttons for token-converter and other dialogs
    
    * better formatting for converter
    
    * more formatting for converter
    
    * Ui layouts (#20)
    
    * updates to tribe-card, tribe-list, tribe-detail layouts
    
    * add blocky to tribe-detail
    
    * build
    
    * polish votestuff
    
    * fix getResults and more polish on votestuff
    
    * add bnt toggle (#30)
    
    * start on tasks functions (#33)

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -1,14 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { of } from 'rxjs/observable/of';
 import { Tribe } from '../tribe/tribe';
-import { Task } from './task';
+import { Task, WorkHistory } from './task';
 import { TASKS } from '../shared/mocks/mock-tasks';
+import { User } from '../user/user';
+import { USERS } from '../shared/mocks/mock-users';
 
 @Injectable()
 export class TaskService {
+  public workHistoryChange: Subject<WorkHistory> = new Subject<WorkHistory>();
 
   constructor() { }
+
+  claimTask(task: Task, user: User){
+    task.state = 'claimed';    
+    task.claimedBy = user || USERS[0];
+  }
+
+  addWork(user: User, comment: string){
+    let workHistory: WorkHistory = {
+      date: new Date(),
+      comment: comment,
+      user: user || USERS[0],
+    }
+    this.workHistoryChange.next(workHistory); 
+  }
+
+  submitTask(task: Task, user: User){
+    task.state = 'pending';    
+    task.submittedBy = user || USERS[0];    
+  }
+
+  quitTask(task: Task){
+    task.state = 'open';
+    task.claimedBy = null;    
+  }
 
   getTasks(tribe: Tribe): Observable<Task[]> {
     return of(TASKS.filter(project => project.tribe === tribe));
--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,6 +1,12 @@
 import { User } from "../user/user";
 import { Tribe } from "../tribe/tribe";
 
+export class WorkHistory {
+  date: Date;
+  comment: string;
+  user: User;
+}
+
 export class Task {
     slug: string;
     title: string;
@@ -9,8 +15,10 @@ export class Task {
     date: Date;
     owner: User;
     reward: number;
-    claimed: boolean;
+    workHistories: WorkHistory[];    
     claimedBy: User;
+    approvedBy: User;
+    submittedBy: User;
     deadline: number;
     tribe: Tribe;
     state: string;  // open/claimed/pending approval/finished  
